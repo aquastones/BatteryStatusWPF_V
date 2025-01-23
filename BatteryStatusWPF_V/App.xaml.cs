640cc14f4637d5ea56dbf88b5f6039c556a61a19
aquas <aquastones@icloud.com>
2025-01-23 17:30:08 +0100

    Add project files.

---
+++ BatteryStatusWPF_V/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace BatteryStatusWPF_V
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ BatteryStatusWPF_V/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace BatteryStatusWPF_V
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartBatteryMonitoring();
+        }
+
+        private void StartBatteryMonitoring()
+        {
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += (s, e) => UpdatePowerStatus();
+            timer.Start();
+
+            UpdatePowerStatus();
+        }
+
+        private void UpdatePowerStatus()
+        {
+            PowerStatus status = SystemInformation.PowerStatus;
+
+            txtChargeStatus.Text = $"Status: {status.BatteryChargeStatus}";
+            txtCharge.Text = $"Charge: {status.BatteryLifePercent:P0}";
+        }
+
+        private void Draggable(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
